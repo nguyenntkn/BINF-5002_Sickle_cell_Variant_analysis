ceca98e3289fb56d52433aba549e1d4368becebf
Nguyen Nguyen <128745593+nguyenntkn@users.noreply.github.com>
2025-04-10 13:54:55 -0300

    Update final_project.sh
    Ensure proper path to folders and files for downloading reference FASTA sequence and raw FASTQ sequence.

--- final_project.sh
+++ final_project.sh
@@ -1,3 +1,4 @@
+# 
 
 function usage {
     echo Project: BINF5002 final project - Genomics variant analysis of sickle cell anemia
@@ -11,6 +12,38 @@ function usage {
     echo conda create --prefix ./bioenv
     echo conda activate /home/user/bioenv
     echo conda install -n bioconda entrez-direct sra-tools fastqc fastp cutadapt trimmomatic multiqc samtools -y
+    exit 1
 }
 
 # usage 
+
+# --------------------------------------------------------------------------------------
+# Relevant file
+
+SRA_RAW=SRR14329362
+REF_ID=NG_059281
+
+RESULTS_DIR=BINF5002_project
+RAW_DATA=${RESULTS_DIR}/rawdata
+
+
+echo Setting up directories...
+mkdir -p $RESULTS_DIR
+mkdir -p $RAW_DATA
+echo Completed setting up directories.
+
+# --------------------------------------------------------------------------------------
+# Download reference fasta sequence
+echo Downloading reference sequence...
+efetch -db nucleotide -id $REF_ID -format fasta > ${RAW_DATA}/reference_${REF_ID}.fasta
+echo Completed downloading reference sequence.
+
+# Download raw sequencing data
+echo Downloading FASTQ file...
+prefetch $SRA_RAW -O $RAW_DATA 
+fastq-dump ./${RAW_DATA}/${SRA_RAW} -O $RAW_DATA
+echo Completed downloading raw sequencing data.
+
+tree $RESULTS_DIR
+
+
