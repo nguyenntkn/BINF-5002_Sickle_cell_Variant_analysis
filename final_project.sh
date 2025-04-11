ca8ceaaec2a96215bbc29d94893ed72ad79e088d
Nguyen Nguyen <128745593+nguyenntkn@users.noreply.github.com>
2025-04-11 15:54:51 -0300

    Added fastp for trimming reads.

--- final_project.sh
+++ final_project.sh
@@ -27,10 +27,11 @@ REF_ID=NG_059281
 RESULTS_DIR=BINF5002_project
 RAW_DIR=${RESULTS_DIR}/rawdata
 QC_REPORT=${RESULTS_DIR}/qc
+TRIMMED_DIR=${RESULTS_DIR}/trimmed
 
 echo Setting up directories...
 mkdir -p $RESULTS_DIR
-mkdir -p $RAW_DATA $QC_REPORT
+mkdir -p $RAW_DATA $QC_REPORT $TRIMMED_DIR
 echo Completed setting up directories.
 
 # --------------------------------------------------------------------------------------
@@ -48,15 +49,40 @@ echo Completed downloading raw sequencing data.
 # Check if the raw FASTQ sequencing file has been downloaded.
 if [ ! -s "$RAW_DIR/${SRA}.fastq" ]; then
     # If the user provided an invalid accession number, technically the efetch command still succeeded, but return an empty file.
-    # -s test if a file existed and is bigger than 0 (empty file). So " ! -s <file> " returns true when the file is empty or not exist. 
+    # -s test if a file existed and is bigger than 0 (empty file). So " ! -s <file> " returns TRUE when the file is empty or not exist. 
     echo "An error has occurred: The result is empty. Please check the accession number."
     exit
 fi
 
 
 # --------------------------------------------------------------------------------------
+# We also need to check if the sequencing is single end or paired end.
+# Paired end reads will contain the /1 /2 identifiers or something similar.
+# However, our sequences in the FASTQ file did not have anything representing that.
+# So it's likely that our reads are single end.
+# This is important for choosing the right tools and flags for bioinformatics commands.
+
+# First do QC check.
 echo Performing QC check on raw FASTQ file...
 fastqc "${RAW_DIR}/${SRA}.fastq" -o $QC_REPORT
+echo QC check completed.
+
+
+
+# Use fastp to filter low quality read. 
+# For our purpose (genomics variant calling), the read quality should be much higher at ~30.
+# Since this is single end read, we will use -i and -o flags (lowercase).
+echo Trimming low quality reads...
+fastp -i "${RAW_DIR}/${SRA}.fastq" -q 30 -o "$TRIMMED_DIR/trimmed_${SRA}.fastq"
+echo Completed trimming low quality reads.
+
+
+# echo Performing QC check on trimmed FASTQ reads...
+
+
+
+
+
 
 
 
